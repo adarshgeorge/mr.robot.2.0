fdacf5bd176910ed46187764fc57f16c8bc8617b
Adarsh K G <42889982+adarshgeorge@users.noreply.github.com>
2021-06-13 22:12:40 +0530

    1st commit working in whatsapp

---
+++ ekm_45.py
@@ -0,0 +1,19 @@
+import requests
+import time
+from datetime import datetime
+
+district = "307"
+today = datetime.today()
+#date = today.strftime("%d-%m-%Y")
+date = "12-06-2021"
+#By pincode
+#url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pin_code}&date={date}"
+url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district}&date={date}"
+data = requests.get(url).json()
+for center in data['sessions']:
+    #print(center['name'], center['min_age_limit'], center['slots'], center['available_capacity'], center['available_capacity_dose1'])
+    #time.sleep(2)
+    if center[ 'min_age_limit' ] == 45 and center[ 'available_capacity_dose1' ] > 0 :
+        print( center['vaccine'] ,'Dose1:' , center['available_capacity_dose1'], center['name'], 'Age limit:', center['min_age_limit'])
+
+time.sleep(10)
---
+++ mrrobot.py
@@ -0,0 +1,33 @@
+import requests 
+import time 
+import twilio
+from twilio.rest import Client
+from decouple import config
+from datetime import datetime 
+account_sid = config('TWILIO_ACCOUNT_SID')
+auth_token = config('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+def dist(district_id): 
+
+    district = district_id 
+    today = datetime.today() 
+    date = today.strftime("%d-%m-%Y") 
+    slots = {} 
+    url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district}&date={date}" 
+
+    data = requests.get(url).json() 
+    for center in data['sessions']: 
+ 
+        if center[ 'min_age_limit' ] == 18 and center[ 'available_capacity_dose1' ] > 0 : 
+            slots[center['name']] = [ {'dose1' : center['available_capacity_dose1']},  {'Age limit': center['min_age_limit']} ] 
+    return(slots) 
+     
+message = client.messages \
+    .create(
+         body='Hi Groot!\n {}'.format(dist(571)),
+         from_='whatsapp:+14155238886',
+         to='whatsapp:+919633131216'
+     )
+
+print(message.sid)  
+print(dist(571)) 
